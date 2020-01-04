f48dbf6ed1e5c4a3c11ce4cbfd2d1b06fd98678f
Ilham Aryasuta <ilhamaryasuta@gmail.com>
2020-01-04 15:20:32 +0700

    SLEk

--- Assets/Resources/Scripts/Game.cs
+++ Assets/Resources/Scripts/Game.cs
@@ -1,7 +1,29 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public struct SampahOAB3 {
+	public List<Sprite> organik;
+	public List<Sprite> anorganik;
+	public List<Sprite> residu;
+}
+
 public class Game : MonoBehaviour {
-	
+	public static Game main;
+	public SampahOAB3 sSampah;
+	public float spawnY;
+	public float maxSpeed = 2f;
+	public SpriteRenderer sungai;
+
+	private void Awake() {
+		if(main == null) {
+			main = this;
+		}
+		else if(main != this) {
+			Destroy(gameObject);
+		}
+
+		spawnY = sungai.bounds.size.y / 2f;
+	}
 }
---
+++ Assets/Resources/Scripts/Sampah.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sampah : MonoBehaviour {
+	private void Update() {
+		Rigidbody2D body = GetComponent<Rigidbody2D>();
+		body.AddForce(Vector2.right);
+		body.velocity = Mathf.Min(body.velocity.magnitude, Game.main.maxSpeed) * body.velocity.normalized;
+	}
+}
