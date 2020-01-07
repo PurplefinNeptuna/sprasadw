a6c259f1a4eec00deac32d8fbac186d48f074a8a
PurplefinNeptuna <ilhamaryasuta@gmail.com>
2020-01-07 09:08:03 +0700

    drag and drop

--- Assets/Resources/Scripts/Sampah.cs
+++ Assets/Resources/Scripts/Sampah.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class Sampah : MonoBehaviour {
 	public int tipeSampah;
+	public bool drag = false;
+	public Rigidbody2D body;
+
+	private void Start() {
+		body = GetComponent<Rigidbody2D>();
+		body.AddTorque(Random.Range(-3f,3f));
+	}
+
 	private void Update() {
-		Rigidbody2D body = GetComponent<Rigidbody2D>();
-		body.AddForce(Vector2.right);
-		body.velocity = Mathf.Min(body.velocity.magnitude, Game.main.maxSpeed) * body.velocity.normalized;
+		if (!drag) {
+			body.AddForce(Vector2.right);
+			body.velocity = Mathf.Min(body.velocity.magnitude, Game.main.maxSpeed) * body.velocity.normalized;
+		}
+		else {
+			body.velocity = Vector2.zero;
+		}
 		if (transform.localPosition.x > 10f) Destroy(gameObject);
+	}
+
+	private void OnMouseDown() {
+		drag = true;
+	}
+
+	private void OnMouseDrag() {
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1 * (Camera.main.transform.position.z));
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+		transform.position = objPosition;
+	}
+
+	private void OnMouseUp() {
+		drag = false;
 	}
 }
